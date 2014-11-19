1fcef618c143bbac3921d02fae190bc4f49dbd1a
Dmitri Shuralyov <shurcooL@gmail.com>
2014-11-18 18:56:00 -0800

    client: Add development scripts.
    These are used for development purposes. Sort of like "make install",
    except using bash scripts.

---
+++ client/install-into-bundle.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+go build -o "$HOME/Dropbox/Applications/Instant Share-dev.app/Contents/MacOS/main" .
---
+++ client/tail-log.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+tail -f "$HOME/Dropbox/Applications/Instant Share-dev.app/Contents/Resources/log.txt"
